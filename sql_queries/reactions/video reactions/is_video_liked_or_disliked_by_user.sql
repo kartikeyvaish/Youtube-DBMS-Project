29b502231f3671315d6bccbe3f5b46b2d9b89b2e
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-01-23 12:26:54 +0530

    updated queries to fix some bugs

--- sql_queries/reactions/video reactions/is_video_liked_or_disliked_by_user.sql
+++ sql_queries/reactions/video reactions/is_video_liked_or_disliked_by_user.sql
@@ -1,5 +1,5 @@
 -- Script to check if a video is liked/disliked by user
 
 SELECT
-    (SELECT EXISTS ( SELECT 1 FROM reactions WHERE user_id = 1 AND video_id = 1 AND reaction_type_id = 1 ) AS is_liked),
-    (SELECT EXISTS ( SELECT 1 FROM reactions WHERE user_id = 1 AND video_id = 1 AND reaction_type_id = 1 ) AS is_disliked);
+    (SELECT EXISTS ( SELECT 1 FROM video_reactions WHERE user_id = 2 AND video_id = 1 AND reaction_type_id = 1 ) AS is_liked),
+    (SELECT EXISTS ( SELECT 1 FROM video_reactions WHERE user_id = 2 AND video_id = 1 AND reaction_type_id = 2 ) AS is_disliked);
